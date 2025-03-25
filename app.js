3941b3af4162cc0641d71438e7adc6ab33f06d41
Mahad-007 <mahadghafoor.07@gmail.com>
2025-03-25 06:13:47 +0500

    Added more buttons

--- app.js
+++ app.js
@@ -7,14 +7,30 @@
 //     list.appendChild(newTask);
 
 //   }
+// let deleteBtn = document.createElement("button");
+// deleteBtn.textContent = "Delete";
+// deleteBtn.onclick = function () {
+//   ul.removeChild(li);
+// };
 const taskInput=document.getElementById('taskinput')
   function addTask(){
     let task = document.getElementById("taskinput").value;
     alert("Task added!");
     const list = document.querySelector("ul");
+    // let checkbox = document.createElement("taskinput");
+    // checkbox.type="checkbox";
     const newTask = document.createElement("li");
     newTask.textContent = task;
     list.appendChild(newTask);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    list.appendChild(deleteBtn);
+    let completeBtn = document.createElement("button");
+    completeBtn.textContent = "Completed";
+    list.appendChild(completeBtn);
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    list.appendChild(editBtn);
   }
   
   const button = document.querySelector("button");
